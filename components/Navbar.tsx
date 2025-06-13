477b1746685dc4346ce259af34fdf9f394101c46
victorpoliushko <victor.poliushko@gmail.com>
2025-06-13 17:56:18 +0300

    trying to reanimate styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,11 @@ import '../app/globals.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="navbar">
       <Link href="/">
         <Image src="/ingest_solution_logo.png" alt="logo" width={274} height={150}/>
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul className="navrbar-ul hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map(link => (
           <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
