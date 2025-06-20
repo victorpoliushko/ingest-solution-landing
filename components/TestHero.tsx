fd6b0acdadcd4189b6e47038908964c510d94bbc
victorpoliushko <victor.poliushko@gmail.com>
2025-06-20 18:37:50 +0300

    tailwind again...

--- components/TestHero.tsx
+++ components/TestHero.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import '../app/globals.css';
 
 const TestHero = () => {
   return (
-    <section className='hero-section'>
-      <div className='hero-map'/>
+    <section className="hero-section">
+      {/* <div className='hero-map'/> */}
+      <h1>A</h1>
+      <div className="hero-body relative z-20 flex flex-1 flex-col xf:w-1/2">
+        <h1 className="bold-52 lg:bold-88">Let's make products together</h1>
+      </div>
     </section>
   )
 }
