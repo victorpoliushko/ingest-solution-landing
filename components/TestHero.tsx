3e82ce078f7ae1cc3a16010b0658c74be82b2bb3
victorpoliushko <victor.poliushko@gmail.com>
2025-06-21 16:31:02 +0300

    WIP hero block

--- components/TestHero.tsx
+++ components/TestHero.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import '../app/globals.css';
+import Image from 'next/image';
 
 const TestHero = () => {
   return (
     <section className="hero-section">
       {/* <div className='hero-map'/> */}
-      <h1>A</h1>
-      <div className="hero-body relative z-20 flex flex-1 flex-col xf:w-1/2">
-        <h1 className="bold-52 lg:bold-88">Let's make products together</h1>
+      <div className="hero-body">
+        <h1 className="hero-title">Let's make products together</h1>
+        <p className="hero-description">
+          We want to deliver the best match for your business needs. High performance, high quality and big experiece are our strengths!
+        </p>
+
+        <div className="hero-outer-block">
+          <div className="hero-innter-block">
+            {Array(5).fill(1).map((_, index) => (
+              <Image
+                src="/star.svg"
+                key={index}
+                alt="star"
+                width={24}
+                height={24} 
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   )
