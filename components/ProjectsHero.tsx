b2d21d18d59cdfa922d899d4395ac464ffbff80d
victorpoliushko <victor.poliushko@gmail.com>
2025-06-28 14:51:06 +0300

    adding projects

--- components/ProjectsHero.tsx
+++ components/ProjectsHero.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
+import invappBg from  "../public/invapp-bg.png";
+import baSiteBg from "../public/BAsite-bg.png";
+
+interface ProjectProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  downloads: string;
+};
+
+const Project = ({
+  backgroundImage,
+  title,
+  subtitle,
+  downloads,
+}: ProjectProps) => {
+  console.log('backgroundImage ', backgroundImage)
+  return <div className="project" style={{ backgroundImage: `url('${backgroundImage}')` }}>Test1</div>;
+};
 
 const ProjectsHero = () => {
   return (
     <section className="projects-hero">
       <div className="projects-hero element">
-        <h1>Test</h1>
+        <Project
+          backgroundImage={invappBg.src}
+          title="InvApp"
+          subtitle="The only investment tracking app you need"
+          downloads="500+ downloads"
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectsHero
+export default ProjectsHero;
