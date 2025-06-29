8be1f86b050d5c641217b389846dec31a763193c
victorpoliushko <victor.poliushko@gmail.com>
2025-06-29 14:51:51 +0300

    add product text

--- components/ProjectsHero.tsx
+++ components/ProjectsHero.tsx
@@ -15,14 +15,25 @@ const Project = ({
   subtitle,
   downloads,
 }: ProjectProps) => {
-  console.log('backgroundImage ', backgroundImage)
-  return <div className="project" style={{ backgroundImage: `url('${backgroundImage}')` }}>Test1</div>;
+  return (<div className="project" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className="project-title-block">
+      <div className="project-title-block-2">
+        <div className="project-title-block-3">
+          <h4>Product</h4>
+        </div>
+        <div className="project-title-text">
+          <h4 className="project-title-text-title">{title}</h4>
+          <p className="project-title-text-subtitle">{subtitle}</p>
+        </div>
+      </div>
+    </div>
+  </div>)
 };
 
 const ProjectsHero = () => {
   return (
     <section className="projects-hero">
-      <div className="projects-hero element">
+      <div className="projects-hero-element">
         <Project
           backgroundImage={invappBg.src}
           title="InvApp"
