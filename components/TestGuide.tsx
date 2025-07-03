47b9dd6c98b2dab4f30025604610f994df4a373a
victorpoliushko <victor.poliushko@gmail.com>
2025-07-03 19:07:15 +0300

    dev route section

--- components/TestGuide.tsx
+++ components/TestGuide.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const TestGuide = () => {
   return (
-    <div>TestGuide</div>
-  )
-}
+    <section className="dev-route-block">
+      <div className="dev-route-pre-title">
+        <p className="dev-route-pre-title-p">How do we work</p>
+        <div className="dev-route-title">
+          <h2 className="dev-route-title-h2">
+            Clear Path to App Development
+          </h2>
+          <p className="dev-route-title-p">
+            We specialize in turning your ideas into fully functional
+            applications. Whether you're starting from scratch or enhancing an
+            existing product, our team is here to guide you through the entire
+            development lifecycle. We handle every stage — from initial planning
+            and UI/UX design, to backend and frontend development, cloud
+            deployment, quality assurance, and post-launch support. Our goal is
+            to help you launch fast, scale efficiently, and maintain long-term
+            success. No matter your industry or business goals, we offer
+            tailored solutions to make your vision a reality. Let's build
+            something great together.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default TestGuide
+export default TestGuide;
