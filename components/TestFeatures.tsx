20c0efcbc8c39612e3156c1ef2a196f0095ad54f
victorpoliushko <victor.poliushko@gmail.com>
2025-07-07 18:06:52 +0300

    adding features component

--- components/TestFeatures.tsx
+++ components/TestFeatures.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const TestFeatures = () => {
   return (
-    <div>TestFeatures</div>
+    <section className="features">
+      <div className="features-outer">
+        <div className="features-inner">
+          <Image
+            src="/dots.jpg"
+            alt="features"
+            width={400}
+            height={1000}
+            className="features-image"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
